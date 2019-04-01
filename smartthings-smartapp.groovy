44fcc4c9df8fee2b44c8165fe4208cf12ccfca84
mike-debney <40560481+mike-debney@users.noreply.github.com>
2019-04-01 20:56:51 +1300

    Update smartthings-smartapp.groovy

--- smartthings-smartapp.groovy
+++ smartthings-smartapp.groovy
@@ -65,7 +65,9 @@ def createChildDevices() {
                 ]).save()
                 child = getChildDevice(child_dni)
             }
-            pollChildDevice(child)
+            if (child) {
+            	pollChildDevice(child)
+            }
         }
     }
 }
